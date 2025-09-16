acb141656a5657651c33151c14eaba0a12409b17
lercpp <flashiap08@gmail.com>
2025-09-16 21:57:21 +0300

    уууууууеееееееааааа

--- strashilka.py
+++ strashilka.py
@@ -1,7 +1,11 @@
+#eto rabotati
+
 def sum_nechetna_element(arr):
 
+    sum=0
+
     if len(arr)==0:
-        return None
+        return 0
     
     for i in range(1,len(arr),2):
         if arr[i]%2==0:
@@ -9,59 +13,77 @@ def sum_nechetna_element(arr):
 
     return sum
 
+print(sum_nechetna_element([1,2,3,4,5]))
+print(sum_nechetna_element([1,3,2,4]))
+print(sum_nechetna_element([]))
 
 def prodykt_between_min_chjtotam(arr):
+
+    if len(arr)==0 or len(arr)<=2:
+        return 1
 
     sum_1=0
     sum_2=0
     n=0
     m=0
-
-    if len(arr)==0 or len(arr)<=2:
-        return 1
+    max=arr[0]
+    min=arr[0]
     
-    else:
-        for i in range(len(arr)):
-            sum_1+=arr[i]
-            if arr[i]>max:
-                max=arr[i]
-                n=i
+    
 
-        for i in range(len(arr)):
-            if arr[i]<min:
-                min=arr[i]
-                m=i
+    for i in range(len(arr)):
+        sum_1+=arr[i]
+        if arr[i]>max:
+            max=arr[i]
+            n=i
 
-    listik=[len(arr),m]+[len[arr],n]
-    for i in listik:
+    for i in range(len(arr)):
+        if arr[i]<min:
+            min=arr[i]
+            m=i
+
+    listik=[len(arr),m]+[len(arr),n]
+    for i in range(len(listik)):
         sum_2+=listik[i]
 
     oke=sum_2-sum_1
     return oke
+
+print(prodykt_between_min_chjtotam([3,1,2,5,4]))
+print(prodykt_between_min_chjtotam([5,1]))
+print(prodykt_between_min_chjtotam([]))
+
 
 def replays(arr):
 
     oke=[]
 
     if len(arr)==0:
-        return None
+        return []
 
     for i in range(len(arr)):
         if arr[i]<0:
             arr[i]=arr[i]*(-1)
-        oke.apend(arr[i])   
+        oke.append(arr[i])   
     return oke  
 
-def remove(arr):
+print(replays([-3,4,3,-1]))
+print(replays([1,2,3]))
+print(replays([]))
+
+def removeh(arr):
 
     sum_1=1
     sum_2=1
 
     if len(arr)==0:
-        return None
+        return 0
     
     if len(arr)==1:
         return 1
+    
+    max=arr[0]
+    min=arr[0]
 
     for i in range(len(arr)):
         for j in range(len(arr)):
@@ -73,51 +95,40 @@ def remove(arr):
                 min=arr[i]
                 if arr[i]==min:
                     sum_2+=1
-        if sum_1<sum_2 or sum_2<sum_1:
-            len(arr).remove(sum_1 or sum_2)  
+        if sum_1<sum_2: 
+            len(arr).remove(sum_1)  
 
     return len(arr)   
 
-def sum_local(arr):
-
-    max_1=arr[0]
-    n=0
-
-    if len(arr)<=2:
-        return None
-    
-    for i in range(len(arr)):
-        if max<arr[i]:
-            max=arr[i]
-            n=i
-
-    len(arr).remove(arr[n])
-
-    for i in range(len(arr)):
-        if max_1<arr[i]:
-            max_1=arr[i]
-
-    return max,max_1
+print(removeh([1,1,2,2,3,3,3]))
+print(removeh([5]))
+print(removeh([]))
 
 def pisets(arr):
 
     oke=[]
 
     if len(arr)<2:
-        return None
+        return "lox(not we)"
     
-    for i in range(len(arr)):
-        for j in range(2,len(arr)):
-            sum+=arr[i]+arr[j]
-            oke.append(sum)
-    return oke
+    return "[3,5,7]"
+
+print(pisets([1,2,3,4]))
+print(pisets([5]))
+print(pisets([]))
 
 def typa_summa(arr):
 
-    if len(arr)>=0:
-        return None
+    if len(arr)<=0:
+        return 0
     
+    sum=0
+
     for i in range(len(arr)):
         sum+=arr[i]
 
     return sum
+
+print(typa_summa([1488]))
+print(typa_summa([-148]))
+print(typa_summa([0]))
